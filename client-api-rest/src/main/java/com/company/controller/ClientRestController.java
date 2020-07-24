1e3919053693a9447a70609b65fc66d83c366a63
Eduard Castro <eduard.cas07@gmail.com>
2020-07-24 10:58:22 -0500

    Add client rest controller

---
+++ client-api-rest/src/main/java/com/company/controller/ClientRestController.java
@@ -0,0 +1,30 @@
+package com.company.controller;
+
+import com.company.model.entity.Client;
+import com.company.model.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = {"http://localhost:4200"})
+@RestController
+@RequestMapping("/api")
+public class ClientRestController {
+
+    private final ClientService clientService;
+
+    @Autowired
+    public ClientRestController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping("/clients")
+    public List<Client> index() {
+        return clientService.findAll();
+    }
+
+}
