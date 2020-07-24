6fb5823e8264bdf2827a1853e1047e36adc1f652
Eduard Castro <eduard.cas07@gmail.com>
2020-07-24 10:55:59 -0500

    Add client entity

---
+++ client-api-rest/src/main/java/com/company/model/entity/Client.java
@@ -0,0 +1,74 @@
+package com.company.model.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="clients")
+public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private Date createdAt;
+
+    public Client() {
+    }
+
+    public Client(Long id, String firstName, String lastName, String email, Date createdAt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
