84960b845b1a1dcba6102128dedffc871eceafb1
Eduard Castro <eduard.cas07@gmail.com>
2020-11-07 00:16:28 -0500

    Add ResponseEntity Client - Back

--- client-api-rest/src/main/java/com/company/controller/ClientRestController.java
+++ client-api-rest/src/main/java/com/company/controller/ClientRestController.java
@@ -4,13 +4,14 @@ import com.company.model.entity.Client;
 import com.company.model.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/clients")
 public class ClientRestController {
 
     private final ClientService clientService;
@@ -20,38 +21,47 @@ public class ClientRestController {
         this.clientService = clientService;
     }
 
-    @GetMapping("/clients")
+    @GetMapping("/")
     public List<Client> index() {
         return clientService.findAll();
     }
 
-    @GetMapping("/clients/{id}")
-    public Client show(@PathVariable Long id) {
-        return clientService.findById(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> show(@PathVariable Long id) {
+        Client client = clientService.findById(id);
+
+        if (client == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(client, HttpStatus.OK);
     }
 
-    @PostMapping("/clients")
+    @PostMapping("/")
     @ResponseStatus(value = HttpStatus.CREATED)
-    public Client create(@RequestBody Client client) {
-        return clientService.create(client);
+    public ResponseEntity<Client> create(@RequestBody Client client) {
+        Client clientNew = clientService.create(client);
+
+        return new ResponseEntity<>(clientNew, HttpStatus.CREATED);
     }
 
-    @PutMapping("/clients/{id}")
-    public Client update(@PathVariable Long id, @RequestBody Client client) {
-
+    @PutMapping("/{id}")
+    public ResponseEntity<Client> update(@PathVariable Long id, @RequestBody Client client) {
         Client current = clientService.findById(id);
 
-        if (current == null) {
-            return null;
+        if (client == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
 
         current.setFirstName(client.getFirstName());
         current.setLastName(client.getLastName());
         current.setEmail(client.getEmail());
-        return clientService.create(current);
+        Client clientUpdated = clientService.create(current);
+
+        return new ResponseEntity<>(clientUpdated, HttpStatus.OK);
     }
 
-    @DeleteMapping("/clients/{id}")
+    @DeleteMapping("/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         clientService.delete(id);
--- client-api-rest/src/main/java/com/company/model/entity/Client.java
+++ client-api-rest/src/main/java/com/company/model/entity/Client.java
@@ -12,10 +12,13 @@ public class Client implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String firstName;
 
+    @Column(nullable = false)
     private String lastName;
 
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(name = "created_at")
